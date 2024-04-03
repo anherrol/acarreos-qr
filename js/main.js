96360f64072a50a648066bd2a585b8c189aba73d
anherrol <anherrol@gmail.com>
2024-04-02 22:37:49 -0600

    Changes to be committed: 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -3,19 +3,6 @@ import { AuthProxy, CatalogsProxy } from "./dataLayer.js";
 if (!previousLoading) {
     var previousLoading = true;
 
-    // Service worker
-    if ('serviceWorker' in navigator) {
-        console.log('Puedes usar los service workers en tu navegador.');
-
-        navigator
-            .serviceWorker
-            .register('./js/sw.js')
-            .then(res => console.log('serviceWorker cargado correctamente.', res))
-            .catch(err => console.log('serviceWorker no se ha podido registrar.', err))
-    } else {
-        console.log('No puede generarse una pwa');
-    }
-
     $(document).ready(function () {
         // login button
         $('#btnLogin').click(function () {
